36c07b46286204c682fae0c271a95968455d5d2d
Krishnamoorthy <krishnamoorthy04102006@gmail.com>
2025-11-04 19:17:42 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Demo{
+    public static void main(String[]args){
+        Scanner s1 = new Scanner(System.in);
+        int a = s1.nextInt();
+        int b = s1.nextInt();
+        int c = s1.nextInt();
+        int d = a+b+c;
+        System.out.println(d);
+        System.out.println("Hello Krishnamoorthy");
+    }
+}
